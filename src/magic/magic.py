310214081eeabc0bff4f9c2b238ee093e8ff4249
andres <afbasto49@ucatolica.du.co>
2025-08-26 17:57:27 -0500

    fibonacci funciona

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -14,7 +14,12 @@ class Magic:
         Returns:
             int: El n-ésimo número de Fibonacci
         """
-        pass
+        if n==0:
+           return 0
+        a, b = 0, 1
+        for _ in range(n):
+            a, b = b, a + b
+        return a
     
     def secuencia_fibonacci(self, n):
         """
