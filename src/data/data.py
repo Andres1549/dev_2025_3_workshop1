f0fa08e3596bc2c24128c931fa819063a771a5b7
andres <afbasto49@ucatolica.du.co>
2025-08-26 17:09:44 -0500

    funciona todo data

--- src/data/data.py
+++ src/data/data.py
@@ -212,4 +212,12 @@ class Data:
         Returns:
             list: Matriz transpuesta
         """
-        pass
+        if not matriz or not matriz[0]:
+            return []
+        filas = len(matriz)
+        columnas = len(matriz[0])
+        transpuesta = [[0] * filas for _ in range(columnas)]
+        for i in range(filas):
+            for j in range(columnas):
+                transpuesta[j][i] = matriz[i][j]
+        return transpuesta
